29bb39a0151285b535a55df191c112948bf3e9a7
Abylkairkhan <a_sarsetaev@kbtu.kz>
2024-05-16 23:46:34 +0500

    bottom navigation implemented

--- app/src/main/java/kz/abyl/arbuz/MainActivity.kt
+++ app/src/main/java/kz/abyl/arbuz/MainActivity.kt
@@ -3,26 +3,35 @@ package kz.abyl.arbuz
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import kz.abyl.arbuz.presentation.bottom_nav.BottomNavigationBar
 import kz.abyl.arbuz.ui.theme.ArbuzTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ArbuzTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                Scaffold(
+                    bottomBar = {
+                        BottomNavigationBar()
+                    }
                 ) {
-                    Greeting("Android")
+                    val padding = it
+                    Greeting(name = "Test", padding = padding)
                 }
             }
         }
@@ -30,17 +39,16 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun Greeting(name: String, padding: PaddingValues, modifier: Modifier = Modifier) {
+    Column(
         modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ArbuzTheme {
-        Greeting("Android")
+            .padding(padding)
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .background(Color.Green),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = name)
     }
 }
---
+++ app/src/main/java/kz/abyl/arbuz/presentation/bottom_nav/BottomNavigationBar.kt
@@ -0,0 +1,68 @@
+package kz.abyl.arbuz.presentation.bottom_nav
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import kz.abyl.arbuz.R
+
+@Composable
+fun BottomNavigationBar(
+
+) {
+
+    val bottomNavItems = listOf(
+        BottomNavItem(
+            name = "Home",
+            route = "home",
+            selectedIcon = R.drawable.home_icon_fill,
+            unselectedIcon = R.drawable.home_icon
+        ),
+        BottomNavItem(
+            name = "Favorite",
+            route = "favorite",
+            selectedIcon = R.drawable.favorite_icon_fill,
+            unselectedIcon = R.drawable.favorite_icon
+        )
+    )
+
+    var selected by remember {
+        mutableIntStateOf(0)
+    }
+
+    NavigationBar(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(74.dp)
+    ) {
+        bottomNavItems.forEachIndexed { index, bottomNavItem ->
+            NavigationBarItem(
+                selected = index == selected,
+                onClick = {
+                    selected = index
+                },
+                icon = {
+                    Icon(
+                        painter = painterResource(
+                            if (index == selected) bottomNavItem.selectedIcon else bottomNavItem.unselectedIcon
+                        ),
+                        contentDescription = ""
+                    )
+                },
+                label = {
+                    Text(text = bottomNavItem.name)
+                }
+            )
+        }
+    }
+}
